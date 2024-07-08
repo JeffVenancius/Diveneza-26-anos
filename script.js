a873f9258e8383d397c8051bcf1cc2d0ebd54e86
JeffVenancius <43701418+JeffVenancius@users.noreply.github.com>
2024-07-08 17:24:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const btn = document.querySelector(".submit_input")
+
+btn.addEventListener("click", event => {
+	event.preventDefault()
+	nome = document.querySelector("#nome")
+	wpp = document.querySelector("#wpp")
+	texto = `Olá! Quero resgatar o meu cupom.%0A
+Nome: ${nome.value}%0A
+Whatsapp: ${wpp.value}`
+	window.location.href = "https://wa.me/553598254687?text=" + texto
+})
