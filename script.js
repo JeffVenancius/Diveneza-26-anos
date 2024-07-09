cc1ca147f9cb0525643e04e4d2ce04afacbb1a1c
JeffVenancius <43701418+JeffVenancius@users.noreply.github.com>
2024-07-09 12:50:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const with_desc = `
-		<div class="placeholder">
-    <section class="description">
-        <p>É com imensa alegria que celebramos 26 anos de excelência e dedicação na Ótica Diveneza.<br>Para marcar essa ocasião especial, estamos trazendo uma inovação revolucionária para você: Lentes com inteligência Artificial. Essas lentes são projetadas para proporcionar a melhor experiência visual, adaptando-se perfeitamente às suas necessidades.</p>
-    </section>
-</div>
-`
-
-	if ( window.screen.width < 1422) {
-		document.querySelector(".placeholder").innerHTML = ""
-	} else {
-		document.querySelector(".placeholder").innerHTML = with_desc
-	}
-
-window.addEventListener('resize', function(event) {
-	if ( window.screen.width < 1422) {
-		document.querySelector(".placeholder").innerHTML = ""
-	} else {
-		document.querySelector(".placeholder").innerHTML = with_desc
-	}
-
-});
-
 const btn = document.querySelector(".submit_input")
 
 btn.addEventListener("click", event => {
